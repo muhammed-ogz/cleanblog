1f1d2a95bef9dd88243c8a340d89f22068938396
muhammed-ogz <muhammetnabioguz@gmail.com>
2024-09-22 16:07:00 +0300

    ejs template engine added. template reorganized.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const PORT = 5000;
 
-app.get('/blog', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+
+app.set("view engine","ejs");
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on : ${PORT} PORT`);
+  console.log(`Click here open to server on : http://127.0.0.1:${PORT}`);
 });
