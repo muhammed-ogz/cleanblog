6fe5920a5e10bbc950cfac82ce8371cc007a54c8
muhammed-ogz <muhammednabioguz@gmail.com>
2025-02-15 01:28:02 +0300

    import to Controllers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = 5000;
 const Text = require('./models/Texts');
+const BlogController = require('./Controllers/BlogController');
+const PageController = require('./Controllers/PageController');
 
 mongoose.connect('mongodb://localhost/cleanblogdb');
 
@@ -11,61 +13,24 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method', {
-  methods: ['POST', 'GET'],
-}));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  const blogs = await Text.find({});
-  res.render('index', {
-    blogs,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/post/:id', async (req, res) => {
-  const blog = await Text.findById(req.params.id);
-  res.render('post', {
-    blog,
-  });
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.get('/post', (req, res) => {
-  res.render('post');
-});
+app.get('/', PageController.getAllPosts);
+app.get('/about', PageController.getAbout);
+app.get('/post/:id', PageController.getPost);
+app.get('/add_post', PageController.getAddPost);
+app.get('/blogs/edit/:id', PageController.getPostInfo);
 
-//Verileri getirme
-app.post('/blogs', async (req, res) => {
-  await Text.create(req.body);
-  res.redirect('/');
-});
+app.post('/blogs', BlogController.sendAddPost);
 
-// Veri Değiştirme editleme için gerekli postun verileri isteniyor.
-app.get('/blogs/edit/:id', async (req, res) => {
-  const blog = await Text.findOne({ _id: req.params.id });
-  res.render('edit', {
-    blog,
-  });
-});
+app.put('/blogs/:id', BlogController.updatePost);
 
-// Verileri Güncelleme
-app.put('/blogs/:id', async (req, res) => {
-  const blog = await Text.findOne({ _id: req.params.id });
-  blog.title = req.body.title;
-  blog.message = req.body.message;
-  blog.save();
-  res.redirect(`/post/${req.params.id}`);
-});
-
-// Veri Silme
-app.delete('/blogs/:id', async (req, res) => {
-  await Text.findOneAndDelete({ _id: req.params.id });
-  res.redirect('/');
-});
+app.delete('/blogs/:id',BlogController.deletePost);
 
 app.listen(PORT, () => {
   console.log(`Server running on : ${PORT} PORT`);
